241b7adec6043f3748c6045267729f040ce8cbf1
Ghata Saraiya <ghatasaraiya@gmail.com>
2020-06-04 22:38:13 +0530

    scraping
    scraping and result code added

--- scrapperfunctions.py
+++ scrapperfunctions.py
@@ -4,6 +4,11 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from configparser import ConfigParser
 import sys
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 
 def configure():
     configur = ConfigParser() 
@@ -17,8 +22,16 @@ def initDriver(url,sector):
     driver = webdriver.Chrome('./chromedriver.exe')
     driver.get(url)
     dropdown=Select(driver.find_element_by_id('competitors-quote-sectors'))
-    dropdown.select_by_visible_text(sector)
-    return(driver)
+    timeout=5
+    try:
+        dropdown.select_by_visible_text(sector)
+        element_present = EC.presence_of_element_located((By.CLASS_NAME, 'next'))
+        WebDriverWait(driver, timeout).until(element_present)
+        return(driver)
+    except TimeoutException:
+        print ("Timed out waiting for page to load")
+        return(None)
+    
 
 def getSymbols(filename):
     symbols=[]
@@ -27,14 +40,83 @@ def getSymbols(filename):
         symbols=[s.strip() for s in symbols]
     return symbols
 
+def createJson(driver):
+    symboldict={}
+    pgNo=1
+    timeout=5
+    while(pgNo<=6):     
+        soup=BeautifulSoup(driver.page_source,'html.parser')
+        table=soup.find("table").find("tbody")
+        rows=table.find_all("tr")
+        for row in rows:
+            cols=row.find_all("td")
+            sym=cols[0].find("div").find("a").text
+            name=cols[1].find("div").find("span").find("span").find("span").text
+            symboldict[sym]=name
+        if(pgNo==6):
+            break
+    
+        try:
+            driver.find_element_by_class_name('next').click()
+            element_present = EC.presence_of_element_located((By.TAG_NAME, 'table'))
+            WebDriverWait(driver, timeout).until(element_present)
+        except TimeoutException:
+            print ("Timed out waiting for page to load")
+            return(None)
+        pgNo+=1 
+    if(len(symboldict)!=503):
+        return(None)
+
+    jsondata=json.dumps([{'Symbol':key,'Name':value} for key,value in symboldict.items()])
+    with open('findata.json','w') as findata:
+        findata.write(jsondata)
+
+    return(symboldict)
+    
+
+def getSymNameJson(query,data,opfilename):
+    result={}
+    for q in query:
+        try:
+            result[q]=data[q]
+        except:
+            result[q]='Name not found'
+    resultjson=json.dumps([{'Symbol':key,'Name':value} for key,value in result.items()])
+    with(opfilename,'w') as opfile:
+        opfile.write(resultjson)
+    return(resultjson)
 
 if __name__ =='__main__':
-    url,sector=configure()
-    driver=initDriver(url,sector)
-    if(len(sys.argv)>1):
-        filename=sys.argv[1]
-    else:
-        filename='symbols.txt'
-    print(getSymbols(filename))
-    driver.close()
+    nargs=len(sys.argv)
+    if(nargs>2):
+        options=sys.argv[2]
+
+        if('init' in options):
+            url,sector=configure()
+            driver=initDriver(url,sector)
+            symdata=createJson(driver)
+            driver.close()
+        else:
+            with open('findata.json','r') as finjson:
+                findata=json.load(finjson)
+                symdata={}
+                for ele in findata:
+                    symdata[ele['Symbol']]=ele['Name']
+        
+        if('ip' in options and nargs>3):
+            ipfilename=sys.argv[3]
+            if('op' in options and nargs==5):
+                opfilename=sys.argv[4]
+            else:
+                opfilename='result.json'
+        elif('op' in options and nargs>3):
+            opfilename=sys.argv[3]
+        else:
+            opfilename='result.json'
+
+    querySymbols=getSymbols(ipfilename)
+    results=getSymNameJson(querySymbols,symdata,opfilename)
+    print(results)
+    
+    
     
--- testfile.py
+++ testfile.py
@@ -1,5 +1,6 @@
 import unittest
 from scrapperfunctions import *
+import json
 
 class TestInits(unittest.TestCase):
 
@@ -8,5 +9,18 @@ class TestInits(unittest.TestCase):
 
     def test_getSymbols(self):
         self.assertEqual(getSymbols('symbols.txt'),['GOOGL','MSFT'])
+
+    def test_createJson(self):
+        url,sector=configure()
+        driver=initDriver(url,sector)
+        result=createJson(driver)
+        self.assertNotEqual(result,None)
+        with open('testdata.json','r') as testjson:
+            testdata=json.load(testjson)
+            testdict={}
+            for ele in testdata:
+                testdict[ele['Symbol']]=ele['Name']
+        self.assertEqual(result,testdict)
+        driver.close()
 if __name__ =='__main__':
     unittest.main()
